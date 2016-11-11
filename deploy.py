9df7cca4ae0b5a5b237585f9505549848be0304c
Rishabh Gupta <r.g.gupta@outlook.com>
2016-11-11 19:04:27 +0530

    Added requests library

--- deploy.py
+++ deploy.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/testing')
+def testing():
+    r = requests.get('https://www.python.org')
+    return str(r.status_code)
