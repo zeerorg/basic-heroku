86b0fc2469ffbf8a61285012900221edcf0ccf37
Rishabh Gupta <r.g.gupta@outlook.com>
2016-11-18 20:47:16 +0530

    Added postgres support

--- deploy.py
+++ deploy.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
