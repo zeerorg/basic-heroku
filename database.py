1e3e53ba1c18e38b399254be0dfdfcfc55188f59
Rishabh Gupta <r.g.gupta@outlook.com>
2016-11-18 21:18:50 +0530

    Added model and database

---
+++ database.py
@@ -0,0 +1,13 @@
+from deploy import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return "Name: " + self.name + "<br>Email: " + self.email
--- deploy.py
+++ deploy.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+from database import User
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,3 +17,18 @@ def index():
 def testing():
     r = requests.get('https://www.python.org')
     return (r.text)
+
+@app.route('/add/<username>/')
+def add_user(username):
+    user = User(username, username+"@gmail.com")
+    db.session.add(user)
+    db.session.commit()
+    return "Added " + username
+
+@app.route('/allusers/')
+def all_users():
+    users = User.query.all()
+    all_of_them = ""
+    for x in users:
+        all_of_them += x + "<br>"
+    return all_of_them
