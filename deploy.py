08ee3140d11d143448bae3b886148548fdbfac40
Rishabh Gupta <r.g.gupta@outlook.com>
2016-11-18 20:52:40 +0530

    fixed import os

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
